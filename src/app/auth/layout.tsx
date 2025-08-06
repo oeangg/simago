df955ccf1dc38552f2feb0344a9001228f8ca810
oeangg <msubhan33@gmail.com>
2025-08-06 17:42:38 +0700

    update cover simago

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -9,8 +9,14 @@ export default function Layout({
   return (
     <section className="w-full h-svh grid grid-cols-1 lg:grid-cols-2 place-items-center">
       {/* <div className="w-full grid grid-cols-1 md:grid-cols-2"> */}
-      <div className="hidden lg:flex w-full h-full relative ">
-        <Image src="/delivery.jpg" alt="image bg" fill />
+      <div className="hidden lg:flex w-full h-full justify-center items-center">
+        <Image
+          src="/simago-cover.jpg"
+          alt="image bg"
+          height={520}
+          width={520}
+          loading="lazy"
+        />
       </div>
       <div className="flex justify-center mx-auto items-center max-w-md w-full">
         {children}
